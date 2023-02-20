c64b3061901c06c94387aaebc51dac76cc37c2c8
Matthew Elliot John-Hall <MEJohnHall@hotmail.co.uk>
2023-02-20 19:28:44 +0000

    Changed target get. Changed back to Vus:10, duration 30s

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
-  stages: [
-    { duration: '30s', target: 20 },
-    { duration: '1m30s', target: 10 },
-    { duration: '20s', target: 0 },
-  ],
+  vus: 10,
+  duration: '30s',
 };
 export default function() {
-  http.get('https://test.k6.io');
+  http.get('https://test-api.k6.io/public/crocodiles/ ');
   sleep(1);
 }
